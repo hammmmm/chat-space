a7fcb99a66227ce3a3793d629a6ca876bc97892a
hammmmm <61265522+hammmmm@users.noreply.github.com>
2020-04-21 15:45:44 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,10 +3,10 @@
 lock '3.13.0'
 
 # Capistranoのログの表示に利用する
-set :application, 'cha-space'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:hammmmm/cha-space.git'
+set :repo_url,  'git@github.com:hammmmm/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
